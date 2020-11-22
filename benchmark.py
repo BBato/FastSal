b11dfffc87b3b6d1ef16cd1557f33fcab9b7a24f
Bartlomiej Baran <32595868+BBato@users.noreply.github.com>
2020-11-22 21:13:33 +0000

    Init

--- benchmark.py
+++ benchmark.py
@@ -2,81 +2,138 @@ import model.fastSal as fastsal
 from utils import load_weight
 import torch
 import time
+import torch.onnx
+import onnxruntime
+from PIL import Image
+import numpy as np
 
-if __name__ == '__main__':
-    coco_c = 'weights/coco_C.pth'  # coco_C
-    coco_a = 'weights/coco_A.pth'  # coco_A
-    salicon_c = 'weights/salicon_C.pth'  # salicon_C
-    salicon_a = 'weights/salicon_A.pth'  # coco_A
+if __name__ == "__main__":
+    coco_c = "weights/coco_C.pth"  # coco_C
+    coco_a = "weights/coco_A.pth"  # coco_A
+    salicon_c = "weights/salicon_C.pth"  # salicon_C
+    salicon_a = "weights/salicon_A.pth"  # coco_A
 
     with torch.no_grad():
 
         x = torch.zeros((1, 3, 192, 256))
 
-        model = fastsal.fastsal(pretrain_mode=False, model_type='A')
+        model = fastsal.fastsal(pretrain_mode=False, model_type="A")
         state_dict, opt_state = load_weight(coco_a, remove_decoder=False)
         model.load_state_dict(state_dict)
+        model.eval()
 
-        model = torch.quantization.quantize_dynamic(
-            model, {torch.nn.LSTM, torch.nn.Linear}, dtype=torch.qint8
-        )
+        # print("Generating sample...")
+        # torch_out = model(x)
+
+        # torch.onnx.export(
+        #     model,  # model being run
+        #     x,  # model input (or a tuple for multiple inputs)
+        #     "exported.onnx",  # where to save the model (can be a file or file-like object)
+        #     export_params=True,  # store the trained parameter weights inside the model file
+        #     opset_version=10,  # the ONNX version to export the model to
+        #     do_constant_folding=True,  # whether to execute constant folding for optimization
+        #     input_names=["input"],  # the model's input names
+        #     output_names=["output"],  # the model's output names
+        # )
+
+        ort_session = onnxruntime.InferenceSession("exported.onnx")
+
+        def to_numpy(tensor):
+            return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
 
         t1 = time.time()
-        for i in range(0,3):
-            y = model(x)
-            print("x")
-        t2 = time.time()
-        interval = (t2-t1)/3
-        fps = 1/interval
-        print("Average FPS for coco_A was "+str(fps))
-        print("Average time for coco_A was "+str(interval))
 
-        model = fastsal.fastsal(pretrain_mode=False, model_type='A')
-        state_dict, opt_state = load_weight(salicon_a, remove_decoder=False)
-        model.load_state_dict(state_dict)
-        model = torch.quantization.quantize_dynamic(
-            model, {torch.nn.LSTM, torch.nn.Linear}, dtype=torch.qint8
-        )
-        t1 = time.time()
-        for i in range(0,3):
-            y = model(x)
-            print("x")
-        t2 = time.time()
-        interval = (t2-t1)/3
-        fps = 1/interval
-        print("Average FPS for salicon_A was "+str(fps))
-        print("Average time for salicon_A was "+str(interval))
+        # compute ONNX Runtime output prediction
+        ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(x)}
+        ort_outs = ort_session.run(None, ort_inputs)
+        y = ort_outs[0]
 
-        model = fastsal.fastsal(pretrain_mode=False, model_type='C')
-        state_dict, opt_state = load_weight(coco_c, remove_decoder=False)
-        model.load_state_dict(state_dict)
-        model = torch.quantization.quantize_dynamic(
-            model, {torch.nn.LSTM, torch.nn.Linear}, dtype=torch.qint8
-        )
-        t1 = time.time()
-        for i in range(0,3):
-            y = model(x)
-            print("x")
-        t2 = time.time()
-        interval = (t2-t1)/3
-        fps = 1/interval
-        print("Average FPS for coco_C was "+str(fps))
-        print("Average time for coco_C was "+str(interval))
+        # Prepare for display
+        y = torch.nn.Sigmoid()(torch.tensor(y))
 
-        model = fastsal.fastsal(pretrain_mode=False, model_type='C')
-        state_dict, opt_state = load_weight(salicon_c, remove_decoder=False)
-        model.load_state_dict(state_dict)
-        model = torch.quantization.quantize_dynamic(
-            model, {torch.nn.LSTM, torch.nn.Linear}, dtype=torch.qint8
-        )
-        t1 = time.time()
-        for i in range(0,3):
-            y = model(x)
-            print("x")
-        t2 = time.time()
-        interval = (t2-t1)/3
-        fps = 1/interval
-        print("Average FPS for salicon_C was "+str(fps))
-        print("Average time for salicon_C was "+str(interval))
+        y = y.detach().numpy()
+        for i, prediction in enumerate(y[:, 0, :, :]):
+            img_data = post_process_png(prediction, camera_image_size)
+            cv2.imshow('img_output_path', img_data)
+            cv2.imshow('original', frame)
 
-    print('All model loaded and tested')
+        # print(img_out_y.shape)
+        # img_out_y = Image.fromarray(np.uint8((img_out_y[0] * 255.0).clip(0, 255)[0]), mode='L')
+
+
+        # # get the output image follow post-processing step from PyTorch implementation
+        # final_img = Image.merge(
+        #     "YCbCr", [
+        #         img_out_y,
+        #         img_cb.resize(img_out_y.size, Image.BICUBIC),
+        #         img_cr.resize(img_out_y.size, Image.BICUBIC),
+        #     ]).convert("RGB")
+
+        # # Save the image, we will compare this with the output image from mobile device
+        # final_img.save("./_static/img/cat_superres_with_ort.jpg")
+
+        t2 = time.time()
+        interval = (t2 - t1)
+        fps = 1 / interval
+        print("Average FPS was " + str(fps))
+        print("Average time was " + str(interval))
+
+        # t1 = time.time()
+        # for i in range(0, 3):
+        #     y = model(x)
+        #     print("x")
+        # t2 = time.time()
+        # interval = (t2 - t1) / 3
+        # fps = 1 / interval
+        # print("Average FPS for coco_A was " + str(fps))
+        # print("Average time for coco_A was " + str(interval))
+
+    #     model = fastsal.fastsal(pretrain_mode=False, model_type="A")
+    #     state_dict, opt_state = load_weight(salicon_a, remove_decoder=False)
+    #     model.load_state_dict(state_dict)
+    #     model = torch.quantization.quantize_dynamic(
+    #         model, {torch.nn.LSTM, torch.nn.Linear}, dtype=torch.qint8
+    #     )
+    #     t1 = time.time()
+    #     for i in range(0, 3):
+    #         y = model(x)
+    #         print("x")
+    #     t2 = time.time()
+    #     interval = (t2 - t1) / 3
+    #     fps = 1 / interval
+    #     print("Average FPS for salicon_A was " + str(fps))
+    #     print("Average time for salicon_A was " + str(interval))
+
+    #     model = fastsal.fastsal(pretrain_mode=False, model_type="C")
+    #     state_dict, opt_state = load_weight(coco_c, remove_decoder=False)
+    #     model.load_state_dict(state_dict)
+    #     model = torch.quantization.quantize_dynamic(
+    #         model, {torch.nn.LSTM, torch.nn.Linear}, dtype=torch.qint8
+    #     )
+    #     t1 = time.time()
+    #     for i in range(0, 3):
+    #         y = model(x)
+    #         print("x")
+    #     t2 = time.time()
+    #     interval = (t2 - t1) / 3
+    #     fps = 1 / interval
+    #     print("Average FPS for coco_C was " + str(fps))
+    #     print("Average time for coco_C was " + str(interval))
+
+    #     model = fastsal.fastsal(pretrain_mode=False, model_type="C")
+    #     state_dict, opt_state = load_weight(salicon_c, remove_decoder=False)
+    #     model.load_state_dict(state_dict)
+    #     model = torch.quantization.quantize_dynamic(
+    #         model, {torch.nn.LSTM, torch.nn.Linear}, dtype=torch.qint8
+    #     )
+    #     t1 = time.time()
+    #     for i in range(0, 3):
+    #         y = model(x)
+    #         print("x")
+    #     t2 = time.time()
+    #     interval = (t2 - t1) / 3
+    #     fps = 1 / interval
+    #     print("Average FPS for salicon_C was " + str(fps))
+    #     print("Average time for salicon_C was " + str(interval))
+
+    # print("All model loaded and tested")
--- from_camera.py
+++ from_camera.py
@@ -7,72 +7,89 @@ import numpy as np
 from PIL import Image
 from generate_img import post_process_png, post_process_probability2
 import time
+import onnxruntime
 
 def convert_vgg_img(src, target_size):
     vgg_img = src
     original_size = vgg_img.size
     if isinstance(target_size, tuple) or isinstance(target_size, list):
-        if (target_size[0] != original_size[1] or target_size[1] != original_size[0]):
+        if target_size[0] != original_size[1] or target_size[1] != original_size[0]:
             vgg_img = vgg_img.resize((target_size[1], target_size[0]), Image.ANTIALIAS)
         elif isinstance(target_size, int):
-            vgg_img = vgg_img.resize((int(original_size[0]/target_size), int(original_size[2]/target_size)), Image.ANTIALIAS)
+            vgg_img = vgg_img.resize(
+                (
+                    int(original_size[0] / target_size),
+                    int(original_size[2] / target_size),
+                ),
+                Image.ANTIALIAS,
+            )
     vgg_img = np.asarray(vgg_img, dtype=np.float32)
     vgg_img = pytorch_normalze(torch.FloatTensor(vgg_img).permute(2, 0, 1) / 255.0)
     return vgg_img, np.asarray(original_size)
-    
 
-if __name__ == '__main__':
 
-    model = fastsal.fastsal(pretrain_mode=False, model_type='A')
-    state_dict, opt_state = load_weight('weights/{}_{}.pth'.format('salicon', 'A'), remove_decoder=False)
-    model.load_state_dict(state_dict)
-    model = torch.quantization.quantize_dynamic(
-        model, {torch.nn.LSTM, torch.nn.Linear}, dtype=torch.qint8
-    )
-    
-    # define a video capture object 
-    vid = cv2.VideoCapture(0) 
+if __name__ == "__main__":
 
-    while(True): 
-        
+    # model = fastsal.fastsal(pretrain_mode=False, model_type='A')
+    # state_dict, opt_state = load_weight('weights/{}_{}.pth'.format('salicon', 'A'), remove_decoder=False)
+    # model.load_state_dict(state_dict)
+    # model = torch.quantization.quantize_dynamic(
+    #     model, {torch.nn.LSTM, torch.nn.Linear}, dtype=torch.qint8
+    # )
+
+    ort_session = onnxruntime.InferenceSession("exported.onnx")
+
+    def to_numpy(tensor):
+        return (
+            tensor.detach().cpu().numpy()
+            if tensor.requires_grad
+            else tensor.cpu().numpy()
+        )
+
+    # define a video capture object
+    vid = cv2.VideoCapture(0)
+
+    while True:
+
         t1 = time.process_time()
 
-        # Capture the video frame by frame 
-        ret, frame = vid.read() 
-        
+        # Capture the video frame by frame
+        ret, frame = vid.read()
+
         # Resize to standard size
         frame = cv2.resize(frame, (320, 240), interpolation=cv2.INTER_AREA)
 
         # Convert to PIL object
-        cv2_im = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        cv2_im = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         pil_im = Image.fromarray(cv2_im)
-        pil_im = pil_im.convert('RGB')
+        pil_im = pil_im.convert("RGB")
         x, camera_image_size = convert_vgg_img(pil_im, (192, 256))
-        x = x[np.newaxis, :,:, :]
+        x = x[np.newaxis, :, :, :]
 
         # Run model
-        y = model(x)
+        ort_inputs = {ort_session.get_inputs()[0].name: to_numpy(x)}
+        ort_outs = ort_session.run(None, ort_inputs)
+        y = ort_outs[0]
 
         # Prepare for display
-        y = torch.nn.Sigmoid()(y)
+        y = torch.nn.Sigmoid()(torch.tensor(y))
 
         y = y.detach().numpy()
         for i, prediction in enumerate(y[:, 0, :, :]):
             img_data = post_process_png(prediction, camera_image_size)
-            cv2.imshow('img_output_path', img_data)
-            cv2.imshow('original', frame)
+            cv2.imshow("img_output_path", img_data)
+            cv2.imshow("original", frame)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'): 
+        if cv2.waitKey(1) & 0xFF == ord("q"):
             break
 
         t2 = time.process_time()
-        interval = (t2-t1)
-        fps = 1/interval
-        print("FPS: "+str(fps))
+        interval = t2 - t1
+        fps = 1 / interval
+        print("FPS: " + str(fps))
 
-    
-    # After the loop release the cap object 
-    vid.release() 
-    
-    # Destroy all the windows 
-    cv2.destroyAllWindows() 
+    # After the loop release the cap object
+    vid.release()
+
+    # Destroy all the windows
+    cv2.destroyAllWindows()
